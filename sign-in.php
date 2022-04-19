461d5d7aeba41e3fa0c87fc4e3d373d218832446
Boni77 <tiffany.dellmann@gmail.com>
2022-04-19 09:54:02 +0200

    css ok

--- sign-in.php
+++ sign-in.php
@@ -10,23 +10,29 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Sign In</title>
 </head>
 
 <body>
-    <?php
-    // c'est içi qu'on affiche l'erreur
-    if (isset($_GET['error'])) {
-        echo "<strong>" . $_SESSION['error'] . "</strong>";
-    }
-    ?>
+    <div class="wrapper">
+        <section>
+            <?php
+            // c'est içi qu'on affiche l'erreur
+            if (isset($_GET['error'])) {
+                echo "<strong>" . $_SESSION['error'] . "</strong>";
+            }
+            ?>
 
-    <form action="add_user.php" method="post">
-        <input type="text" placeholder="Votre login" name="username" required>
-        <input type="password" placeholder="Mdp" name="password" required>
-        <input type="password" placeholder="Confirme mdp" name="pwdverif" required>
-        <input type="submit" value="inscription">
-    </form>
+            <form action="add_user.php" method="post">
+                <h1>Inscrivez-vous ! ou <a href='index.php' class='lien'>connectez-vous içi</a></h1>
+                <p><input type="text" placeholder="Votre login" name="username" required></p>
+                <p><input type="password" placeholder="Mdp" name="password" required></p>
+                <p><input type="password" placeholder="Confirme mdp" name="pwdverif" required></p>
+                <p><input type="submit" value="Enregistrer"></p>
+            </form>
+        </section>
+    </div>
 </body>
 
 </html>
